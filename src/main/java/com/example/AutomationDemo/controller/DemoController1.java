4b613fb85ce6410222a4f0966e08aec6d6101f9c
PriyankaGanu <ladpriyanka24@gmail.com>
2025-04-10 15:31:24 +0530

    Update DemoController1.java

--- src/main/java/com/example/AutomationDemo/controller/DemoController1.java
+++ src/main/java/com/example/AutomationDemo/controller/DemoController1.java
@@ -1,29 +1,22 @@
 package com.example.AutomationDemo.controller;
-import org.slf4j.LoggerFactory;
-
-import org.slf4j.Logger;
-
-
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController1 {
-    private static final Logger LOGGER = LoggerFactory.getLogger(DemoController1.class);
-
-
+   
     @GetMapping("/getStringData1")
     public String getStringData1()
     {
-        LOGGER.info("function getStringData1 called...");
+        System.out.println("function getStringData1 called...");
         return "Welcome to Automation Demo !!";
     }
 
     @GetMapping("/getStringData2")
     public String getStringData2()
     {
-        LOGGER.info("function getStringData2 called...");
+        System.out.println("function getStringData2 called...");
         return "Welcome to Automation Demo !!";
     }
 
